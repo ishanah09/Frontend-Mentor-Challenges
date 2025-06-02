c7802124459fb77cc280ddc8748f840856b09f90
ishanah09 <ishanahmad0906@gmail.com>
2025-06-02 17:39:26 +0530

    upload new project

---
+++ PROJECT 05-Mortgage-Repayment-Calculator/src/App.jsx
@@ -0,0 +1,9 @@
+import Calculator from "./component/calculator";
+
+export default function App() {
+  return (
+    <main className="bg-[hsl(202,86%,94%)] w-full min-h-screen flex ">
+      <Calculator></Calculator>
+    </main>
+  );
+}
